e591f4453ee8873e7628d559cc0090c6c68351fc
BosEriko <bos.eriko@gmail.com>
2019-10-16 08:35:26 +0800

    add a japanese symbol

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,27 @@
 <template>
   <header>
     <div class="container mx-auto">
-      Header
+      <h1>
+        {{ site_title_symbole }}
+      </h1>
     </div>
   </header>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap');
+
+header h1 {
+  font-family: 'Noto Sans JP', sans-serif;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      site_title_symbole: `${process.env.siteTitleSymbol}`
+    }
+  }
+}
+</script>
