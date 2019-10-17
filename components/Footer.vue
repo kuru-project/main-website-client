6fb9e362d55178ece59dc4270fdb495ba39d23b6
BosEriko <bos.eriko@gmail.com>
2019-10-18 07:32:14 +0800

    add a few style for the homepage's footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,18 @@
 <template>
-  <footer>
-    <div class="container mx-auto">
-      Footer
+  <footer class="bg-gray-700">
+    <div class="text-center text-white py-5 mt-5">
+      {{ site_title }} &copy; {{ current_year }}
     </div>
   </footer>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      site_title: `${process.env.siteTitle}`,
+      current_year: new Date().getFullYear()
+    }
+  }
+}
+</script>
