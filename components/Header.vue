3eca3a33d86f1f9acc8ef7a43f996dea3c4e2cef
BosEriko <bos.eriko@gmail.com>
2019-10-22 08:29:06 +0800

    fix the header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-  <header :class="header_style.join(' ')">
+  <header
+    class="
+      bg-kuru
+      border-b
+      border-kuru-dark
+      border-solid
+      mb-5
+      text-white
+    "
+  >
     <div class="container mx-auto py-3">
       <h1 class="font-bold text-xl">
         {{ site_title_symbole }}
@@ -20,15 +29,7 @@ header h1 {
 export default {
   data() {
     return {
-      site_title_symbole: `${process.env.siteTitleSymbol}`,
-      header_style: [
-        `bg-kuru`,
-        `border-b`,
-        `border-kuru-dark`,
-        `border-solid`,
-        `mb-5`,
-        `text-white`
-      ]
+      site_title_symbole: `${process.env.siteTitleSymbol}`
     }
   }
 }
