538bfc186141eef770f2e37eb3c6f6b4b63c6ab2
BosEriko <bos.eriko@gmail.com>
2020-01-06 21:03:54 +0800

    WIP - divide the login and register page

---
+++ src/pages/login.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+import Page from '../components/Page'
+import Container from '../components/Container'
+import IndexLayout from '../layouts'
+
+const LoginPage = () => (
+  <IndexLayout>
+    <Page>
+      <Container>
+        <div>
+          <h3>Register</h3>
+          <p>...</p>
+        </div>
+        <Link to="/">Go back to the homepage</Link>
+      </Container>
+    </Page>
+  </IndexLayout>
+)
+
+export default LoginPage
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -5,15 +5,10 @@ import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
 
-const AuthenticationPage = () => (
+const RegisterPage = () => (
   <IndexLayout>
     <Page>
       <Container>
-        <h2>Authentication Page</h2>
-        <div>
-          <h3>Login</h3>
-          <p>...</p>
-        </div>
         <div>
           <h3>Register</h3>
           <p>...</p>
@@ -24,4 +19,4 @@ const AuthenticationPage = () => (
   </IndexLayout>
 )
 
-export default AuthenticationPage
+export default RegisterPage
