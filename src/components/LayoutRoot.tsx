f2fa61311f1fa58be202c954436e983951176e54
BosEriko <bos.eriko@gmail.com>
2020-01-11 15:08:57 +0800

    WIP - add redux boilerplate (not finished)

--- src/components/LayoutRoot.tsx
+++ src/components/LayoutRoot.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
+import { Provider } from 'react-redux'
 import { Global, css } from '@emotion/core'
 import styled from '@emotion/styled'
 import normalize from '../styles/normalize'
+import { store } from '../redux'
 
 const StyledLayoutRoot = styled.div`
   display: flex;
@@ -14,10 +16,10 @@ interface LayoutRootProps {
 }
 
 const LayoutRoot: React.FC<LayoutRootProps> = ({ children, className }) => (
-  <>
+  <Provider store={store}>
     <Global styles={() => css(normalize)} />
     <StyledLayoutRoot className={className}>{children}</StyledLayoutRoot>
-  </>
+  </Provider>
 )
 
 export default LayoutRoot
---
+++ src/redux.ts
@@ -0,0 +1,35 @@
+import { combineReducers, createStore } from 'redux'
+
+// Constants
+const ADD_OPERATION_NUMBER = `ADD_OPERATION_NUMBER`
+
+// Actions
+export const addOperationNumber = (operationNumber: any) => ({
+  type: ADD_OPERATION_NUMBER,
+  operationNumber
+})
+
+// Reducers
+export const currentData = (state: any, action: any) => {
+  switch (action.type) {
+    case ADD_OPERATION_NUMBER:
+      return state + 1
+
+    default:
+      return state
+  }
+}
+
+// Combine Reducers
+export const reducers = combineReducers({
+  currentData
+})
+
+// Configure Store
+export function configureStore(initialState: any) {
+  const store = createStore(reducers, initialState)
+  return store
+}
+
+// Export Store
+export const store = configureStore({})
