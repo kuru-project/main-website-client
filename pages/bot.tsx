e8baf6d99f2c4b2311c242df5b0977dbf5b871b4
Bos Eriko Reyes <bos.eriko@gmail.com>
2020-03-16 00:05:04 +0800

    [Finishes #81] Add a bot page
    # Story Title
    
    [Add a bot page](https://github.com/kuru-project/main-website-client/issues/81)
    
    # Changes made
    
    - Create bot page
    - Add bot page to the Navigation component
    
    # How does the solution address the problem
    
    This PR will add a bot page

---
+++ pages/bot.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+function BotPage() {
+  return (
+    <div>
+      <Head>
+        <title>Bot - Kuru Anime</title>
+      </Head>
+      <div>
+        Bot
+      </div>
+    </div>
+  )
+}
+
+export default BotPage
