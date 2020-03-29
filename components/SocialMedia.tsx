00da02a0d7a3399b7f516965a02452d746ad66ae
Rin Minase <rin.black.raison.detre@gmail.com>
2020-03-29 18:20:41 +0800

    modify to use tailwind class

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,19 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
+const styles = {
+  discord: {
+    color: '#7289da'
+  },
+  facebook: {
+    color: '#3b5998'
+  },
+  twitter: {
+    color: '00acee'
+  },
+}
+
 function SocialMedia() {
   return (
     <ul>
       <li>
-        <span><FontAwesomeIcon icon={faTwitter} style={{ marginRight: 5, color: '#00acee '}} /></span>
+        <span><FontAwesomeIcon icon={faTwitter} className="mr-1" style={styles.twitter} /></span>
         <a href="http://twitter.kuru-anime.com/" target="_blank">Twitter</a>
       </li>
       <li>
-        <span><FontAwesomeIcon icon={faFacebook} style={{ marginRight: 5, color: '#3b5998' }} /></span>
+        <span><FontAwesomeIcon icon={faFacebook} className="mr-1" style={styles.facebook} /></span>
         <a href="http://facebook.kuru-anime.com/" target="_blank">Facebook</a>
       </li>
       <li>
-        <span><FontAwesomeIcon icon={faDiscord} style={{ marginRight: 5, color: '#7289da' }} /></span>
+        <span><FontAwesomeIcon icon={faDiscord} className="mr-1" style={styles.discord} /></span>
         <a href="http://discord.kuru-anime.com/" target="_blank">Discord</a>
       </li>
     </ul>
