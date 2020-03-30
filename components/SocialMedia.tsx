0be954dbefd886c968112c4068c15448cc01bd61
Kristian Alunan <rin.black.raison.detre@gmail.com>
2020-03-30 08:23:20 +0800

    [Finishes #85] Install Font Awesome
    # Story Title
    
    [Install Font Awesome](https://github.com/kuru-project/main-website-client/issues/85)
    
    # Changes made
    
    - Add font awesome dependencies
    - Import font awesome in social media component
    - Applied icons to social media component
    
    # How does the solution address the problem
    
    This PR will add font awesome icons to the project
    
    # Linked issues
    
    Resolves #85

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,9 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const styles = {
+  discord: {
+    color: '#7289da'
+  },
+  facebook: {
+    color: '#3b5998'
+  },
+  twitter: {
+    color: '00acee'
+  },
+}
+
 function SocialMedia() {
   return (
     <ul>
-      <li><a href="http://twitter.kuru-anime.com/" target="_blank">Twitter</a></li>
-      <li><a href="http://facebook.kuru-anime.com/" target="_blank">Facebook</a></li>
-      <li><a href="http://discord.kuru-anime.com/" target="_blank">Discord</a></li>
+      <li>
+        <span><FontAwesomeIcon icon={faTwitter} className="mr-1" style={styles.twitter} /></span>
+        <a href="http://twitter.kuru-anime.com/" target="_blank">Twitter</a>
+      </li>
+      <li>
+        <span><FontAwesomeIcon icon={faFacebook} className="mr-1" style={styles.facebook} /></span>
+        <a href="http://facebook.kuru-anime.com/" target="_blank">Facebook</a>
+      </li>
+      <li>
+        <span><FontAwesomeIcon icon={faDiscord} className="mr-1" style={styles.discord} /></span>
+        <a href="http://discord.kuru-anime.com/" target="_blank">Discord</a>
+      </li>
     </ul>
   )
 }
